c45bd39eaca872a3f7701c26be2ce5cdfc6b46f6
Lucas Augusto <labmarinho@gmail.com>
2020-03-16 20:07:29 -0300

    Add files via upload

---
+++ Aula2_CEP/BuscaCEP.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+#define max 9
+typedef struct _Endereco Endereco;
+
+struct _Endereco
+{
+	char logradouro[72];
+	char bairro[72];
+	char cidade[72];
+	char uf[72];
+	char sigla[2];
+	char cep[8];
+	char lixo[2];
+};
+
+int main(int argc, char**argv)
+{
+	FILE *f;
+	Endereco e;
+	int qt;
+	int c;
+
+	if(argc != 2)
+	{
+		fprintf(stderr, "USO: %s [CEP]", argv[0]);
+		return 1;
+	}
+
+	c = 0;
+	printf("Tamanho da Estrutura: %ld\n\n", sizeof(Endereco));
+	f = fopen("cep.dat","r");
+	qt = fread(&e,sizeof(Endereco),1,f);
+	while(qt > 0)
+	{
+		c++;
+		if(strncmp(argv[1],e.cep,8)==0)
+		{
+			printf("%.72s\n%.72s\n%.72s\n%.72s\n%.2s\n%.8s\n",e.logradouro,e.bairro,e.cidade,e.uf,e.sigla,e.cep);
+			break;
+		}
+		qt = fread(&e,sizeof(Endereco),1,f);		
+	}
+	void peneira(int *vet, int raiz, int fundo);
+
+void heapsort(int *vet, int n) {
+	int i, tmp;
+
+	for (i = (n / 2); i >= 0; i--) {
+		peneira(vet, i, n - 1);
+	}
+
+	for (i = n-1; i >= 1; i--) {
+		tmp = vet[0];
+		vet[0] = vet[i];
+		vet[i] = tmp;
+		peneira(vet, 0, i-1);
+	}
+}
+
+void peneira(int *vet, int raiz, int fundo) {
+	int pronto, filhoMax, tmp;
+
+	pronto = 0;
+	while ((raiz*2 <= fundo) && (!pronto)) {
+		if (raiz*2 == fundo) {
+			filhoMax = raiz * 2;
+		}
+		else if (vet[raiz * 2] > vet[raiz * 2 + 1]) {
+			filhoMax = raiz * 2;
+		}
+		else {
+			filhoMax = raiz * 2 + 1;
+		}
+
+	if (vet[raiz] < vet[filhoMax]) {
+		tmp = vet[raiz];
+		vet[raiz] = vet[filhoMax];
+		vet[filhoMax] = tmp;
+		raiz = filhoMax;
+    }
+	else {
+      pronto = 1;
+	}
+  }
+}
+
+int main() {
+	int vetor[max] = {7, 10, 13, 100, 98, 3, 55, 43, 1};
+	int i;
+	heapsort(vetor,max);
+	for (i = 0; i < max; i++) {
+		printf("%d ", vetor[i]);
+	}
+	return(0);
+}
+
+	printf("Total Lido: %d\n", c);
+	fclose(f);
+}
+
